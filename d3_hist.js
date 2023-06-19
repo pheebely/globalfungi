9ef7450fa71bf930d28d5a4625acae88d01b7307
pheebely <pheebely@gmail.com>
2023-06-19 22:31:06 +0200

    Updates

--- d3_hist.js
+++ d3_hist.js
@@ -32,8 +32,47 @@ const parseRow = (d) => {
 
 const csvdata =
   "https://raw.githubusercontent.com/pheebely/globalfungi/main/globalfungi_merged.csv";
-d3.csv(csvdata, parseRow).then(function (data) {
+// d3.csv(csvdata, parseRow).then(function (data) {
+//   console.log(data);
+
+//   // X axis
+//   const x = d3
+//     .scaleBand()
+//     .range([0, width])
+//     .domain(data.map((d) => d.continent))
+//     .padding(0.2);
+//   svg
+//     .append("g")
+//     .attr("transform", `translate(0, ${height})`)
+//     .call(d3.axisBottom(x))
+//     .selectAll("text")
+//     .attr("transform", "translate(-10,0)rotate(-45)")
+//     .style("text-anchor", "end");
+
+//   // Add Y axis
+//   const y = d3.scaleLinear().domain([0, 5000]).range([height, 0]);
+//   svg.append("g").call(d3.axisLeft(y));
+
+//   // Bars
+//   const totalcount = d3.count(data, (d) => d.id);
+
+//   console.log(totalcount);
+
+//   svg
+//     .selectAll("mybar")
+//     .data(data)
+//     .join("rect")
+//     .attr("x", (d) => x(d.continent))
+//     .attr("y", (d) => y(d.year_end))
+//     .attr("width", x.bandwidth())
+//     .attr("height", (d) => height - y(d.year_end))
+//     .attr("fill", "#69b3a2");
+// });
+
+// Number of samples per continent
+d3.json("continents.json").then(function (data) {
   console.log(data);
+
   // X axis
   const x = d3
     .scaleBand()
@@ -49,7 +88,7 @@ d3.csv(csvdata, parseRow).then(function (data) {
     .style("text-anchor", "end");
 
   // Add Y axis
-  const y = d3.scaleLinear().domain([0, 60000]).range([height, 0]);
+  const y = d3.scaleLinear().domain([0, 20000]).range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
   // Bars
@@ -58,8 +97,10 @@ d3.csv(csvdata, parseRow).then(function (data) {
     .data(data)
     .join("rect")
     .attr("x", (d) => x(d.continent))
-    .attr("y", (d) => y(d.year_end))
+    .attr("y", (d) => y(d.total))
     .attr("width", x.bandwidth())
-    .attr("height", (d) => height - y(d.year_end))
+    .attr("height", (d) => height - y(d.total))
     .attr("fill", "#69b3a2");
 });
+
+//To-do: add function for showing total number when hover
