3294c80f31f2ed17d97573448183228e4a788fe7
pheebely <pheebely@gmail.com>
2023-06-20 21:24:10 +0200

    Updates on layout, layer controls, popups

--- d3_hist.js
+++ d3_hist.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+const margin = { top: 30, right: 30, bottom: 80, left: 85 },
   width = 460 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
 
@@ -35,43 +35,9 @@ const csvdata =
 // d3.csv(csvdata, parseRow).then(function (data) {
 //   console.log(data);
 
-//   // X axis
-//   const x = d3
-//     .scaleBand()
-//     .range([0, width])
-//     .domain(data.map((d) => d.continent))
-//     .padding(0.2);
-//   svg
-//     .append("g")
-//     .attr("transform", `translate(0, ${height})`)
-//     .call(d3.axisBottom(x))
-//     .selectAll("text")
-//     .attr("transform", "translate(-10,0)rotate(-45)")
-//     .style("text-anchor", "end");
-
-//   // Add Y axis
-//   const y = d3.scaleLinear().domain([0, 5000]).range([height, 0]);
-//   svg.append("g").call(d3.axisLeft(y));
-
-//   // Bars
-//   const totalcount = d3.count(data, (d) => d.id);
-
-//   console.log(totalcount);
-
-//   svg
-//     .selectAll("mybar")
-//     .data(data)
-//     .join("rect")
-//     .attr("x", (d) => x(d.continent))
-//     .attr("y", (d) => y(d.year_end))
-//     .attr("width", x.bandwidth())
-//     .attr("height", (d) => height - y(d.year_end))
-//     .attr("fill", "#69b3a2");
-// });
-
 // Number of samples per continent
 d3.json("continents.json").then(function (data) {
-  console.log(data);
+  // console.log(data);
 
   // X axis
   const x = d3
@@ -82,17 +48,25 @@ d3.json("continents.json").then(function (data) {
   svg
     .append("g")
     .attr("transform", `translate(0, ${height})`)
+    .attr("color", "#f2f2f2")
     .call(d3.axisBottom(x))
     .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
 
+  svg
+    .append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2)
+    .attr("y", height + margin.top + 50)
+    .text("Continent/Ocean");
+
   // Add Y axis
   const y = d3.scaleLinear().domain([0, 20000]).range([height, 0]);
-  svg.append("g").call(d3.axisLeft(y));
+  svg.append("g").attr("color", "#f2f2f2").call(d3.axisLeft(y));
 
   // Bars
-  svg
+  const bars = svg
     .selectAll("mybar")
     .data(data)
     .join("rect")
@@ -100,7 +74,43 @@ d3.json("continents.json").then(function (data) {
     .attr("y", (d) => y(d.total))
     .attr("width", x.bandwidth())
     .attr("height", (d) => height - y(d.total))
-    .attr("fill", "#69b3a2");
-});
+    .attr("fill", "#69b3a2")
+    .style("opacity", 0.7)
+    .append("title")
+    .text(function (d) {
+      return "Total: " + d.total;
+    });
 
-//To-do: add function for showing total number when hover
+  svg
+    .append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 30)
+    .attr("x", -height / 2 + margin.top)
+    .text("No. of Samples");
+
+  //To-do: add function for showing total number when hover
+  // create a tooltip
+  // const tip = d3
+  //   .select("d3_hist")
+  //   .append("div")
+  //   .attr("class", "tooltip")
+  //   .style("opacity", 0);
+
+  // //Add events to bars
+  // bars
+  //   .on("mouseover", function (d) {
+  //     tip
+  //       .style("opacity", 1)
+  //       .html("Total: " + d.total)
+  //       .style("left", d3.pageX - 10 + "px")
+  //       .style("top", d3.pageY - 10 + "px");
+  //     bars.style("opacity", 1);
+  //     console.log(d);
+  //   })
+
+  //   .on("mouseout", function (d) {
+  //     tip.style("opacity", 0);
+  //     bars.style("opacity", 0.7);
+  //   });
+});
---
+++ script.js
@@ -0,0 +1,164 @@
+// var map = L.map('map').setView([48.138666, 11.575366], 5);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// maxZoom: 18,
+// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
+// }).addTo(map);
+
+const key = "HSFYhxikAmTFWmRDoLeS";
+// const map = L.map('map').setView([48.138666, 11.575366],2.5);
+const positron = L.maptilerLayer({
+  apiKey: key,
+  style: `https://api.maptiler.com/maps/f7cf6abe-9c77-42d0-93b0-3106db0f53f3/style.json?key=${key}`, // optional
+});
+const satellite = L.maptilerLayer({
+  apiKey: key,
+  style: `https://api.maptiler.com/maps/satellite/style.json?key=HSFYhxikAmTFWmRDoLeS`,
+});
+
+const avgtemptile = L.tileLayer.wms(
+  "https://storage.googleapis.com/zhenjiang-li/2022 July Average Daily Air Temperature at 2m/{z}/{x}/{y}",
+  {
+    layers: "2022 July Average Daily Air Temperature at 2m",
+    format: "image/png",
+    transparent: true,
+    noWrap: true,
+  }
+);
+
+const nitrogen = L.tileLayer.wms(
+  "https://maps.isric.org/mapserv?map=/map/nitrogen.map",
+  {
+    layers: "nitrogen_0-5cm_mean",
+    format: "image/png",
+    transparent: true,
+    noWrap: true,
+  }
+);
+
+const globalfungiwms = L.tileLayer.wms("http://localhost:8080/geoserver/wms/", {
+  layers: "sample_list_asia1206",
+  format: "image/png",
+  transparent: true,
+  noWrap: true,
+  version: "1.1.0",
+});
+
+const amusMarker = {
+  radius: 4,
+  fillColor: "#f81934",
+  color: "#fff",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8,
+};
+
+const amus = new L.GeoJSON.AJAX("amanita_muscaria.geojson", {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, amusMarker);
+  },
+  onEachFeature: onEachFeature,
+});
+
+// Add pop up for each point feature
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.id) {
+    layer.bindPopup(
+      "<h3>" +
+        "Sample ID: " +
+        feature.properties.id +
+        "</h3>" +
+        "<p>" +
+        "Paper ID: " +
+        feature.properties.paper_id +
+        "<br>" +
+        "Continent: " +
+        feature.properties.continent +
+        "<br>" +
+        "Sample type: " +
+        feature.properties.sample_type +
+        "<br>" +
+        "Biome: " +
+        feature.properties.biome +
+        "</p>"
+    );
+  } else {
+  }
+}
+
+// Molleweide with map's pixel origin at coordinate (0, 0)
+//Use Proj4 to change projection
+// const crs = new L.Proj.CRS(
+//   "ESRI:54009",
+//   "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
+//   {
+//     resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+//     origin: [0, 0],
+//   }
+// );
+
+const map = new L.map("map", {
+  // crs: crs,
+  minZoom: 2,
+  maxZoom: 10, //crs.options.resolutions.length
+  continuousWorld: true,
+  worldCopyJump: false,
+  layers: [positron, amus],
+}).setView([0, 0], 2);
+
+L.control.scale().addTo(map);
+
+// Layer Controls
+
+map.createPane("labels");
+map.getPane("labels").style.zIndex = 650;
+map.getPane("labels").style.pointerEvents = "none";
+
+const baseMaps = {
+  "Map Tiler Dark": positron,
+  Satellite: satellite,
+};
+
+const overlayMaps = {
+  "Mean Annual Temperature": avgtemptile,
+  "Nitrogen 0-5cm depth (cg/kg)": nitrogen,
+  // "All Samples": globalfungiwms,
+  "Amanita muscaria Samples": amus,
+};
+
+const layerControl = L.control
+  .layers(baseMaps, overlayMaps, { collapsed: false })
+  .addTo(map);
+
+uri =
+  "https://maps.isric.org/mapserv?map=/map/nitrogen.map&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=nitrogen_0-5cm_mean&format=image/png&STYLE=default";
+
+const positronLabels = L.maptilerLayer({
+  apiKey: key,
+  pane: "labels",
+  style: `https://api.maptiler.com/maps/ff2aabac-aeec-4475-88cb-0b7528028c78/style.json?key=${key}`,
+  // optional
+}).addTo(map);
+
+// Add and remove layers
+
+var nitrolegend;
+
+map.on("overlayadd", function (e) {
+  // Switch to the nitrogen legend...
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  nitrolegend = L.wmsLegend(uri);
+
+  if (e.name === "Nitrogen 0-5cm depth (cg/kg)") {
+    nitrolegend.addTo(map);
+    console.log(e);
+  } else if (map.removeControl(nitrolegend));
+});
+
+map.on("overlayremove", function (e) {
+  if (e.name === "Nitrogen 0-5cm depth (cg/kg)") {
+    map.removeControl(nitrolegend);
+    console.log(e);
+  }
+});
